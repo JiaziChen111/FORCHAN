ea40442e77f93074aa2c5ef2cf2a7e56a5a1ef74
chandlerlester <clester3@uoregon.edu>
2020-01-29 11:07:39 -0800

    Added code for geometric means

--- Simple_Code/Geometric_Means/geomeans.f90
+++ Simple_Code/Geometric_Means/geomeans.f90
@@ -2,28 +2,39 @@ PROGRAM geometric_means
 IMPLICIT NONE 
 
 ! Declare space for up to ten variables, N-number of variables, and the mean 
-REAL:: v1,v2,v3,v4,v5,v6,v7,v8,v9,v10, mean, mult, N2  
-REAL:: numbers(10) !space for 10 numbers in array 
+REAL:: v1,mult, mean, N2   
+REAL:: numbers(10) !dynamic array
 INTEGER:: i, N 
 
 PRINT *, "Please enter up to ten positive numbers"
-READ *, v1,v2,v3,v4,v5,v6,v7,v8,v9, v10
 
-numbers = (/v1,v2,v3,v4,v5,v6,v7,v8,v9,v10 /)
+DO i = 1,10
+    PRINT *, "Enter a positive number, to exit loop enter a non-positive number"
+    READ *, v1 
 
-if ( ANY(numbers<=0)) then 
-    PRINT *, "Please enter up to TEN POSTIVE NUMBERS" 
-    READ *, v1,v2,v3,v4,v5,v6,v7,v8,v9, v10
-end if 
+    IF (v1 > 0) THEN 
+	numbers(i)= v1 
+    ELSE IF (v1<=0) THEN 
+	EXIT  
+    END IF 	
 
-N =10 
+END DO
+
+
+
+N = i  
 mult = 1 
 
-do i=1,N 
-   mult=mult*numbers(i)
-end do  
+DO i=1,N 
+   IF  (numbers(i) > 0) THEN 
+       mult=mult*numbers(i)
+   END IF 
+END DO   
 
-N2 = N
+N2 = N-1 !can't do integer divison
+
+PRINT *,N2  
+ 
 mean = mult**(1/N2) 
 
 PRINT *, "The Geometric mean of these numbers is", mean 
